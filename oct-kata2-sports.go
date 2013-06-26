2233138a4afdb33eacc28b20bc4a26d94fc2ffd9
Matthew Thorley <padwasabimasala on gmail>
2013-06-26 11:23:48 -0600

    clean up

--- oct-kata2-sports.go
+++ oct-kata2-sports.go
@@ -13,10 +13,11 @@ import (
 func main() {
   scanner := bufio.NewScanner(os.Stdin)
   r, err := regexp.Compile(`\d+\. `)
-   if err != nil {
-        fmt.Printf("There is a problem with you regexp.\n")
-        return
-    }
+
+  if err != nil {
+    fmt.Printf("There is a problem with you regexp.\n")
+    return
+  }
 
   var (
     least_dif float64 = 1000.0
@@ -24,36 +25,31 @@ func main() {
   )
 
   for scanner.Scan() {
-      if r.MatchString(scanner.Text()) == true {
-        // fmt.Println(scanner.Text())  
-        // s := regexp.MustCompile(`\s+`).Split(scanner.Text(),-1)
-        // t := strings.Replace(scanner.Text(), " ", "|", -1)
-        // s := strings.Split(t, "|")
+    if r.MatchString(scanner.Text()) == true {
+      s := strings.Fields(scanner.Text())
 
-        s := strings.Fields(scanner.Text())
-
-        f_goals, err := strconv.ParseFloat(s[6],64)
-        if err != nil {
-          fmt.Println(err)
-        }
-
-        a_goals, err := strconv.ParseFloat(s[8],64)
-        if err != nil {
-          fmt.Println(err)
-        }
-        
-        dif := math.Abs(f_goals - a_goals)
-
-        if dif < least_dif {
-          least_dif = dif
-          team_name = s[1]
-        }
+      f_goals, err := strconv.ParseFloat(s[6],64)
+      if err != nil {
+        fmt.Println(err)
       }
+
+      a_goals, err := strconv.ParseFloat(s[8],64)
+      if err != nil {
+        fmt.Println(err)
+      }
+      
+      dif := math.Abs(f_goals - a_goals)
+
+      if dif < least_dif {
+        least_dif = dif
+        team_name = s[1]
+      }
+    }
   }
   
   fmt.Println(team_name)
 
   if err := scanner.Err(); err != nil {
-      fmt.Fprintln(os.Stderr, "reading standard input:", err)
+    fmt.Fprintln(os.Stderr, "reading standard input:", err)
   }
 }
