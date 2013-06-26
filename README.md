299848c13d001c322004c04f0232c029658f18b5
Matthew Thorley <padwasabimasala on gmail>
2013-06-26 11:19:42 -0600

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+## About
+
+From time to time at O.C. Tanner we take a moment to stop "regular" work and sharpen the saw. One of the ways we do this is programming Katas. These are small programming excercises that we do a group to see what we learn from each other.
+
+For this exercise three of us teamed up to see if we could do it in go https://golang.org, a language none of us had any experience with.
+
+Here's the example code from one we did in June of 2013.
+
+## Kata Objective
+
+The file football.dat contains the results from the English Premier League for 2001/2. The columns labeled ‘F’ and ‘A’ contain the total number of goals scored for and against each team in that season (so Arsenal scored 79 goals against opponents, and had 36 goals scored against them). Write a program to print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+
+## Thanks
+
+Special thanks to @solap for being an awesome CTO and for putting this together and to @octethankennelly and @stevepeterson for pairing on this with me.
---
+++ kata2-sports.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "regexp"
+  "strings"
+  "strconv"
+  "math"
+)
+
+func main() {
+  scanner := bufio.NewScanner(os.Stdin)
+  r, err := regexp.Compile(`\d+\. `)
+   if err != nil {
+        fmt.Printf("There is a problem with you regexp.\n")
+        return
+    }
+
+  var (
+    least_dif float64 = 1000.0
+    team_name = ""
+  )
+
+  for scanner.Scan() {
+      if r.MatchString(scanner.Text()) == true {
+        // fmt.Println(scanner.Text())  
+        // s := regexp.MustCompile(`\s+`).Split(scanner.Text(),-1)
+        // t := strings.Replace(scanner.Text(), " ", "|", -1)
+        // s := strings.Split(t, "|")
+
+        s := strings.Fields(scanner.Text())
+
+        f_goals, err := strconv.ParseFloat(s[6],64)
+        if err != nil {
+          fmt.Println(err)
+        }
+
+        a_goals, err := strconv.ParseFloat(s[8],64)
+        if err != nil {
+          fmt.Println(err)
+        }
+        
+        dif := math.Abs(f_goals - a_goals)
+
+        if dif < least_dif {
+          least_dif = dif
+          team_name = s[1]
+        }
+      }
+  }
+  
+  fmt.Println(team_name)
+
+  if err := scanner.Err(); err != nil {
+      fmt.Fprintln(os.Stderr, "reading standard input:", err)
+  }
+}
